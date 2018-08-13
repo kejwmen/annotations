3a6d0d240b149bed1c151dc57e2980860791acdd
Michael Moravec <me@majkl.me>
2018-08-13 17:04:28 +0200

    Scope + ReferenceResolver (WIP)

---
+++ lib/Doctrine/Annotations/Parser/ReferenceResolver.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Annotations\Parser;
+
+use Doctrine\Annotations\Parser\Ast\Reference;
+use function class_exists;
+
+/**
+ * @internal
+ */
+final class ReferenceResolver
+{
+    public function resolve(Reference $reference, Scope $scope) : string
+    {
+        $class = $this->resolveFullyQualifiedName($reference, $scope);
+
+        assert(class_exists($class), 'class not found');
+
+        return $class;
+    }
+
+    private function resolveFullyQualifiedName(Reference $reference, Scope $scope) : string
+    {
+        $identifier = $reference->getIdentifier();
+
+        if ($reference->isFullyQualified()) {
+            return $identifier;
+        }
+
+        $namespace = $scope->getSubject()->getNamespaceName();
+
+        if ($namespace === '') {
+            return $imports[$identifier] ?? $identifier;
+        }
+
+        if (strpos($identifier, '\\') === false) {
+            return $imports[$identifier] ?? $namespace . '\\' . $identifier;
+        }
+
+        return $namespace . '\\' . $identifier;
+    }
+}
